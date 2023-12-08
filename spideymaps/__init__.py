1de9569690122c25f2f1057123da25865ebf00be
Daniel Foust <djfoust@umich.edu>
2023-12-08 11:30:35 -0500

    Fix relative imports

--- spideymaps/__init__.py
+++ spideymaps/__init__.py
@@ -1,3 +1,3 @@
-# from spideymaps_calculation import *
-# from spideymaps_read import *
-# from spideymaps_rendering import *
+from .spideymaps_calculation import *
+from .spideymaps_read import *
+from .spideymaps_rendering import *
